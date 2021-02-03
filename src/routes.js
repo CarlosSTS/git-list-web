d91f08c7e8995f39e0c388e0590a1fdb586399e2
Francisco Carlos Ferreira <franciscocarlosferreira@Franciscos-MacBook-Pro.local>
2021-02-03 16:30:28 -0300

    Finished Application

---
+++ src/routes.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {BrowserRouter,Switch,Route} from "react-router-dom" 
+
+import Main from "./pages/Main" 
+import Repository from "./pages/Repository" 
+
+function Routes() {
+  return (
+<BrowserRouter>
+<Switch>
+  <Route exact path="/" component={Main} />
+  <Route  path="/Repository/:repository" component={Repository} />
+
+</Switch>
+</BrowserRouter>
+  )
+}
+
+export default Routes;
